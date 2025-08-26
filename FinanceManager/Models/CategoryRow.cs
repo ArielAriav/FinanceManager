9d68c18365e31c76f0405de1c47b2178b64fc8f9
ariel <arielar@ac.sce.ac.il>
2025-08-26 14:22:19 +0300

    Creation and design of the filter button on the main page for expenses/income

---
+++ FinanceManager/Models/CategoryRow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Models
+{
+    public class CategoryRow
+    {
+        public int Id { get; }
+        public string CategoryName { get; }
+        public string TypeText { get; }
+        public string AmountText { get; }
+        public string? BudgetText {  get; }
+        public bool HasBudget => !string.IsNullOrEmpty(BudgetText);
+
+        public  CategoryRow(int id, string categoryName, string typeText, string amountText, string? budgetText)
+        {
+            Id = id;
+            TypeText = typeText;
+            CategoryName = categoryName;
+            AmountText = amountText;
+            BudgetText = budgetText;
+        }
+    }
+}
